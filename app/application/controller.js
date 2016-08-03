4adbe2609031801cc81b03e334c8ef249ec1bfab
neilthawani@gmail.com <neilthawani@gmail.com>
2016-08-03 16:40:29 -0500

    Completed bonus for great-tipper

--- app/application/controller.js
+++ app/application/controller.js
@@ -5,18 +5,18 @@ export default Ember.Controller.extend({
     mealCost: null,
     tipPercentage: 20,
     tipAmount: Ember.computed('mealCost', 'tipPercentage', {
-    	get: function() {
-    		let mealCost = this.get('mealCost'),
-    			tipPercentage = this.get('tipPercentage'),
-    			tipAmount = mealCost * (tipPercentage / 100);
+        get: function() {
+            let mealCost = this.get('mealCost'),
+                tipPercentage = this.get('tipPercentage'),
+                tipAmount = mealCost * (tipPercentage / 100);
 
-    		return truncateDecimal(tipAmount) || 0;
-    	}
+            return truncateDecimal(tipAmount) || 0;
+        }
     }),
     totalAmount: Ember.computed('mealCost', 'tipAmount', {
         get: function() {
-        	let mealCost = parseFloat(this.get('mealCost'), 2),
-        		tipAmount = this.get('tipAmount');
+            let mealCost = parseFloat(this.get('mealCost'), 2),
+                tipAmount = this.get('tipAmount');
 
             return mealCost + tipAmount || 0;
         }
@@ -31,13 +31,36 @@ export default Ember.Controller.extend({
         }
     }),
 
+    previousAmounts: Ember.computed('savedAmountsToggle', {
+        get: function() {
+            return JSON.parse(window.localStorage.getItem('previousAmounts')) || [];
+        }
+    }),
+    savedAmountsToggle: false,
+
     // when the user clicks a button, the tip percentage is automatically set to that amount
     actions: {
         setTipPercentage: function(value) {
             this.set('tipPercentage', value);
+        },
+        // Challenge: The user wants to be able to dial in an amount and a tip and save it to an array and see all of their recent meals in a table 
+        // (bonus points if you store the data using `localStorage`).
+        saveAmounts: function() {
+            let previousAmounts = JSON.parse(window.localStorage.getItem('previousAmounts')) || [],
+                mealCost = this.get('mealCost'),
+                tipPercentage = this.get('tipPercentage'),
+                tipAmount = this.get('tipAmount'),
+                totalAmount = this.get('totalAmount');
+
+            previousAmounts.push({
+                mealCost: mealCost,
+                tipPercentage: tipPercentage,
+                tipAmount: tipAmount,
+                totalAmount: totalAmount
+            });
+
+            window.localStorage.setItem('previousAmounts', JSON.stringify(previousAmounts));
+            this.toggleProperty('savedAmountsToggle');
         }
     }
-
-    // Challenge: The user wants to be able to dial in an amount and a tip and save it to an array and see all of their recent meals in a table 
-    // (bonus points if you store the data using `localStorage`).
 });
